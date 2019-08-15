43bbb6d0bb4594ff94fd2a2cf046f85bd30427b5
Hamdan Rijali <hamdanrijali@gmail.com>
2019-08-15 13:37:49 +0700

    Initial commit

---
+++ src/test/java/com/hamdan/warpin/WarpinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hamdan.warpin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WarpinApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
